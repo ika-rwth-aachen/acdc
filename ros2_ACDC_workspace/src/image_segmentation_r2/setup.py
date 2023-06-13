a8f507fbe20d5a67e6649e32bf454a4bde6fd4c1
ba2sakal <sakalbatuhan1@gmail.com>
2023-06-13 13:30:46 +0200

    image_segmentation add

---
+++ ros2_ACDC_workspace/src/image_segmentation_r2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'image_segmentation_r2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rosuser',
+    maintainer_email='rosuser@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
