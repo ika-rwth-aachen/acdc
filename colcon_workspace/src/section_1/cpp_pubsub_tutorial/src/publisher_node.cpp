bf23baf5054edab8d7f4ff5a45bfa16ff2c43727
Batuhan Sakal <sakal@i1900121.ika.rwth-aachen.de>
2023-09-14 18:04:16 +0200

    Update Pub Sub Tutorial final push

--- colcon_workspace/src/section_1/cpp_pubsub_tutorial/src/publisher_node.cpp
+++ colcon_workspace/src/section_1/cpp_pubsub_tutorial/src/publisher_node.cpp
@@ -1,16 +1,22 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright (c) 2022 Institute for Automotive Engineering of RWTH Aachen University
+
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
 
 // Necessary standard library headers
 #include <chrono>
@@ -38,6 +44,7 @@ public:
   {
     // Decleare and Load the parameters
     declareParameters();
+
     loadParameters();
 
     // Create a publisher with the topic name "topic", using String messages, and a queue size of 10
@@ -60,8 +67,8 @@ private:
   void loadParameters()
   {
     // Load the parameter values
-    this->get_parameter("base_message", base_message);
-    this->get_parameter("topic", topic_name);
+    base_message = this->get_parameter("base_message").as_string();
+    topic_name = this->get_parameter("topic").as_string();
   }
 
   // This function is called every 500ms by the timer
