5d6766abc51ab404e2161f68689cc7a9fe6d5fb1
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2023-09-26 16:27:37 +0200

    modify r2_run.sh to attach to running container if container is already running; adapt to image

--- docker/r2_run.sh
+++ docker/r2_run.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
 # in order to be able to use this script install:
 # pip install docker-run-cli
 DIR="$(cd -P "$(dirname "$0")" && pwd)"
-docker-run --gpus all --volume $(dirname "$DIR"):/home/rosuser --image rwthika/acdc:ros2-cuda-tf
+if docker ps --format '{{.Names}}' | grep -q "acdc"; then
+    docker-run --name acdc
+else
+    docker-run --gpus all --volume $(dirname "$DIR"):/docker-ros/ws --image rwthika/acdc:ros2-cuda-tf --name acdc
+fi
