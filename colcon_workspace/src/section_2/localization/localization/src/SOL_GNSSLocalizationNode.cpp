57c8c51a83615647e01ba5198d673c3037cd37e1
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2023-10-04 17:14:12 +0200

    update task numbering in solution file

--- colcon_workspace/src/section_2/localization/localization/src/SOL_GNSSLocalizationNode.cpp
+++ colcon_workspace/src/section_2/localization/localization/src/SOL_GNSSLocalizationNode.cpp
@@ -98,14 +98,14 @@ void GNSSLocalizationNode::gnssCallback(sensor_msgs::msg::NavSatFix::UniquePtr m
 bool GNSSLocalizationNode::projectToUTM(const double& latitude, const double& longitude, geometry_msgs::msg::PointStamped& utm_point)
 {
   try {
-    // START TASK 1 CODE HERE
+    // START TASK 2 CODE HERE
     int zone;
     bool northp;
     utm_point.header.frame_id="utm";
     GeographicLib::UTMUPS::Forward(latitude, longitude, zone, northp, utm_point.point.x, utm_point.point.y);
     // return true if succesful
     return true;
-    // END TASK 1 CODE HERE
+    // END TASK 2 CODE HERE
   } catch (GeographicLib::GeographicErr& e) {
     RCLCPP_WARN_STREAM(this->get_logger(), "Tranformation from WGS84 to UTM failed: " << e.what());
     return false;
@@ -123,12 +123,12 @@ bool GNSSLocalizationNode::projectToUTM(const double& latitude, const double& lo
 bool GNSSLocalizationNode::transformPoint(const geometry_msgs::msg::PointStamped& input_point, geometry_msgs::msg::PointStamped& output_point, const std::string& output_frame)
 {
   try {
-    // START TASK 2 CODE HERE
+    // START TASK 3 CODE HERE
     geometry_msgs::msg::TransformStamped tf = tf_buffer_->lookupTransform(output_frame, input_point.header.frame_id, input_point.header.stamp);
     tf2::doTransform(input_point, output_point, tf);
     // return true if succesful
     return true;
-    // END TASK 2 CODE HERE
+    // END TASK 3 CODE HERE
   } catch (tf2::TransformException& ex) {
     RCLCPP_WARN_STREAM(this->get_logger(), "Tranformation from '" << input_point.header.frame_id << "' to '" << output_frame << "' is not available!");
     return false;
@@ -144,7 +144,7 @@ bool GNSSLocalizationNode::transformPoint(const geometry_msgs::msg::PointStamped
  */
 void GNSSLocalizationNode::estimateGNSSHeading(const geometry_msgs::msg::PointStamped& current_point, const geometry_msgs::msg::PointStamped& last_point, geometry_msgs::msg::PoseStamped& output_pose)
 {
-    // START TASK 3 CODE HERE
+    // START TASK 4 CODE HERE
     // calculate the yaw angle from two sequential gnss-points
     double dx = current_point.point.x-last_point.point.x;
     double dy = current_point.point.y-last_point.point.y;
@@ -158,7 +158,7 @@ void GNSSLocalizationNode::estimateGNSSHeading(const geometry_msgs::msg::PointSt
     tf2::Quaternion q;
     q.setRPY(0, 0, yaw);
     output_pose.pose.orientation = tf2::toMsg(q);
-    // END TASK 3 CODE HERE
+    // END TASK 4 CODE HERE
 }
 
 /**
@@ -253,7 +253,7 @@ void GNSSLocalizationNode::posePrediction(geometry_msgs::msg::PoseStamped& pose,
   // now perform the transformation of the translation into map coordinates, by using the yaw of the vehicle in map coordinates
   double initial_yaw;
   getYawFromQuaternion(initial_yaw, orientation);
-  // START TASK 4 CODE HERE
+  // START TASK 5 CODE HERE
   double alpha = std::atan2(delta_translation.y, delta_translation.x);
   double beta = initial_yaw - alpha;
   double translation_magnitude = std::sqrt(std::pow(delta_translation.x, 2.0)+std::pow(delta_translation.y, 2.0));
@@ -262,7 +262,7 @@ void GNSSLocalizationNode::posePrediction(geometry_msgs::msg::PoseStamped& pose,
   // Apply dx and dy (in map coordinates) to the position
   pose.pose.position.x += map_dx;
   pose.pose.position.y += map_dy;
-  // END TASK 4 CODE HERE
+  // END TASK 5 CODE HERE
 }
 
 /**
