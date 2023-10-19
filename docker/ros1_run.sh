c6b18eaf87543f0565bae0da81641ff881bc7676
till.beemelmanns@ika.rwth-aachen.de <depulsor@gmail.com>
2023-10-19 15:23:44 +0200

    bug fix run scripts

--- docker/ros1_run.sh
+++ docker/ros1_run.sh
@@ -3,8 +3,8 @@
 # in order to be able to use this script install:
 # pip install docker-run-cli
 DIR="$(cd -P "$(dirname "$0")" && pwd)"
-if docker ps --format '{{.Names}}' | grep -q "acdc"; then
-    docker-run --name acdc
+if docker ps --format '{{.Names}}' | grep -q "acdc_ros1"; then
+    docker-run --name acdc_ros1
 else
     docker-run --no-gpu --volume $(dirname "$DIR"):/home/rosuser/ws --image rwthika/acdc:ros1 --workdir="/home/rosuser/ws/catkin_workspace" --name acdc_ros1 
 fi
--- docker/ros2_run.sh
+++ docker/ros2_run.sh
@@ -3,8 +3,8 @@
 # in order to be able to use this script install:
 # pip install docker-run-cli
 DIR="$(cd -P "$(dirname "$0")" && pwd)"
-if docker ps --format '{{.Names}}' | grep -q "acdc"; then
-    docker-run --name acdc
+if docker ps --format '{{.Names}}' | grep -q "acdc_ros2"; then
+    docker-run --name acdc_ros2
 else
     docker-run --no-gpu --volume $(dirname "$DIR"):/home/rosuser/ws --image rwthika/acdc:ros2 --name acdc_ros2
 fi
